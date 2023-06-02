eb3bc0a84161f1c2f8a2f0bf0c1c5c46016d1664
ugistelmokaitis <hello@ugistelmokaitis.com>
2023-06-02 23:56:19 +1000

    Add(eslint, prettier & stylelint): strict linting configs

---
+++ prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 2,
+    printWidth: 80,
+    useTabs: false,
+    semi: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always',
+    proseWrap: 'never',
+};
